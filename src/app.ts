c3864d34326d6028f8339de5149daec1d5dad0b9
Kravchuk <majerkravchuk@gmail.com>
2018-10-24 22:44:59 +0300

    Add bluebird

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import express, { Request, Response } from "express";
+import logger from "./util/logger";
+import { MONGODB_URI } from "./util/secrets";
 
 const app = express();
 
@@ -7,5 +9,7 @@ app.set("port", process.env.PORT || 3000);
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!!");
 });
+
+console.log(MONGODB_URI);
 
 export default app;
